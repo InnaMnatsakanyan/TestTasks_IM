8a0d1335c67f6ef569083a2e5c6320cbee9e919b
innam <innam@HHPC003>
2022-09-19 02:52:42 +0400

    Add project files.

---
+++ Task 1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GetProcess
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // receive input
+            Console.WriteLine("Enter splitted by space:\r\nThe process name\r\nMaximum lifetime(in minutes)\r\nMonitoring frequency(in minutes)");
+            string input = Console.ReadLine();
+            string[] split = input.Split(' ');
+            string ProcessName = split[0];
+            int MaxLifetime = Convert.ToInt32(split[1]);
+            int Frequency = Convert.ToInt32(split[2]);
+
+            // set timer
+            var timer = new Timer(MonitorProcesses, null, 0, Frequency * 60000);
+            Console.ReadKey();
+
+            void MonitorProcesses(object o)
+            {
+                // store the processes in list
+                Process[] list = Process.GetProcessesByName(ProcessName);
+
+                // output the processes and their duration times
+                foreach (var process in list)
+                {
+                    Console.WriteLine(process.ProcessName + " " + process.TotalProcessorTime.TotalMinutes);
+                }
+                Console.ReadLine();
+
+                // check lifetime, and if exceeds, kill
+                for (int i = 0; i < list.Length; i++)
+                {
+                    if (list[i].TotalProcessorTime.TotalMinutes > MaxLifetime)
+                        list[i].Kill();
+                }
+            }
+        }
+    }
+}
---
+++ Task 2.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+
+namespace JobCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompareJobCount();
+        }
+
+        static void CompareJobCount()
+        {
+            // input number of expected jobs
+            Console.WriteLine("Enter the number of expected jobs:");
+            int expectedJobCount = Convert.ToInt32(Console.ReadLine());
+
+            // start web driver
+            IWebDriver webDriver = new ChromeDriver(@"C:\Users\innam");
+            webDriver.Url = "https://cz.careers.veeam.com/vacancies";
+
+            // maximize window
+            webDriver.Manage().Window.Maximize();
+
+            // go to Research & Development page
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            webDriver.FindElement(By.XPath("//a[text()='Departments']")).Click();
+            webDriver.FindElement(By.XPath("//*[@id=\"root\"]/div/header/div[1]/div/div/div/nav/div/div[2]/div/div/div/div/div/ul[1]/a[3]")).Click();
+
+            // scroll down to all jobs part
+            Actions actions = new(webDriver);
+            actions.MoveToElement(webDriver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[1]/div[3]/section/div/div[2]/a/div[1]/div[2]/a")));
+            actions.Perform();
+
+            // count all the number of jobs
+            webDriver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[1]/div[3]/section/div/div[2]/a/div[1]/div[2]/a")).Click();
+            int jobCount = webDriver.FindElements(By.XPath("//*[@id=\"root\"]/div/div[1]/div/div/div[2]/div/a")).Count;
+
+            // close the web driver
+            webDriver.Close();
+
+            if (jobCount.CompareTo(expectedJobCount) == 0)
+                Console.WriteLine("THE NUMBER OF JOBS IS VALID");
+            else
+                Console.WriteLine("THE NUMBER OF JOBS IS NOT VALID. There are found " + jobCount + " jobs.");
+        }
+    }
+}
